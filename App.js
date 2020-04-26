9cd3b9607e7eebd05f422f285fbc5970b154a030
Tetsuya Asano <tetsuyaasano@TetsuyanoMacBook-puro.local>
2020-04-26 22:06:00 +0900

    Store memos with cloud store

--- App.js
+++ App.js
@@ -5,10 +5,14 @@ import firebase from 'firebase';
 import MemoListScreen from './src/screens/MemoListScreen';
 import MemoDetailScreen from './src/screens/MemoDetailScreen';
 import MemoEditScreen from './src/screens/MemoEditScreen';
+import MemoCreateScreen from './src/screens/MemoCreateScreen';
 import LoginScreen from './src/screens/LoginScreen';
 import SignupScreen from './src/screens/SignupScreen';
 
 import ENV from './env.json';
+
+// eslint-disable-next-line
+require("firebase/firestore");
 
 const firebaseConfig = {
   apiKey: ENV.FIREBASE_API_KEY,
@@ -23,11 +27,12 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 const App = createStackNavigator({
-Login:      { screen: LoginScreen },
+  Login:      { screen: LoginScreen },
   Signup:     { screen: SignupScreen },
   Home:       { screen: MemoListScreen },
   MemoDetail: { screen: MemoDetailScreen },
   MemoEdit:   { screen: MemoEditScreen },
+  MemoCreate: { screen: MemoCreateScreen },
 }, {
   defaultNavigationOptions: {
     headerTitle: 'Memot',
